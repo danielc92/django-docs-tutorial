dda818a55466a541581f0b422130c46e701d71da
Daniel Corcoran <danielc.92@outlook.com>
2019-05-08 11:00:08 +1000

    Fixed routing issue

--- dans_django_proj/urls.py
+++ dans_django_proj/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from polls.views import polls_index, polls_detail, polls_results, polls_vote
+from polls.views import polls_index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/details/<int:question_id>/', polls_detail, name='polls_detail'),
-    path('polls/results/<int:question_id>/', polls_detail, name='polls_results'),
-    path('polls/vote/<int:question_id>/', polls_detail, name='polls_vote'),
-    path('polls/', polls_detail, name='polls_index')
-]
+    path('polls/', polls_index),
+    path('admin/', admin.site.urls)
+    ]
+    
--- polls/views.py
+++ polls/views.py
@@ -2,17 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def polls_index(request):
-	return HttpResponse("Polls Index")
-
-def polls_detail(request, question_id):
-	return HttpResponse("You're looking at question {}"\
-		.format(question_id))
-
-def polls_results(request, question_id):
-	return HttpResponse("Your're looking at the results of question {}"\
-		.format(question_id))
-
-def polls_vote(request, question_id):
-	return HttpResponse("You're voting on question {}"\
-		.format(question_id))
+def polls_index(*args, **kwargs):
+	return HttpResponse("<h1>Polls Index</h1>")
