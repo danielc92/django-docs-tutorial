bab6fb93877a1b0c28832f84b3b3eeddd38bf81c
Daniel Corcoran <danielc.92@outlook.com>
2019-05-08 10:01:51 +1000

    Addded some views

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def detail(request, question_id):
+	return HttpResponse("You're looking at question {}"\
+		.format(question_id))
+
+def results(request, question_id):
+	return HttpResponse("Your're looking at the results of question {}"\
+		.format(question_id))
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question {}"\
+		.format(question_id))
