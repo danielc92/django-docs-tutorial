d3972453aba9e81f595dda54e630e02ac0e11f0a
Daniel Corcoran <danielc.92@outlook.com>
2019-05-10 16:16:09 +1000

    Added author and category to model and form class

--- polls/forms.py
+++ polls/forms.py
@@ -3,6 +3,16 @@ from .models import Question, Choice
 
 class RawQuestionForm(forms.Form):
 	question_text = forms.CharField()
+	category = forms.ChoiceField(choices=(
+        ('POL', 'Politics'),
+        ('SCI', 'Science'),
+        ('FOO', 'Food'),
+        ('TEC', 'Technology'),
+        ('FAS', 'Fashion'),
+        ('ADV', 'Advice'),
+        ('OTH', 'Other')
+    )  )
+	author = forms.CharField()
 
 class RawChoiceForm(forms.Form):
 	choice_text = forms.CharField()
--- polls/models.py
+++ polls/models.py
@@ -2,11 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
+
+    choices = (
+        ('POL', 'Politics'),
+        ('SCI', 'Science'),
+        ('FOO', 'Food'),
+        ('TEC', 'Technology'),
+        ('FAS', 'Fashion'),
+        ('ADV', 'Advice'),
+        ('OTH', 'Other')
+    )   
+
     question_text = models.CharField(max_length=200)
+    category = models.CharField(max_length=3,
+                                choices=choices,
+                                default='OTH')
+    author = models.CharField(max_length=60, default='Anonymous')
     pub_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-    	return self.question_text
+        return self.question_text
 
 
 class Choice(models.Model):
@@ -15,4 +30,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-    	return self.choice_text
+        return self.choice_text
