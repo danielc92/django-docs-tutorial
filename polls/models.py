af4759143f3e09d95f122aedd0fe61d74cdb473f
Daniel Corcoran <danielc.92@outlook.com>
2019-07-05 15:41:49 +1000

    Replaced all models with  many-to-many models

--- polls/models.py
+++ polls/models.py
@@ -1,35 +1,35 @@
 from django.db import models
 
 # Create your models here.
-choices = (
-        ('Politics', 'Politics'),
-        ('Science', 'Science'),
-        ('Food', 'Food'),
-        ('Technology', 'Technology'),
-        ('Fashion', 'Fashion'),
-        ('Advice', 'Advice'),
-        ('Other', 'Other')
-    )   
+class Tag(models.Model):
 
-class Question(models.Model):
+    tag_name = models.CharField(max_length=50)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.tag_name
+
+
+class Poll(models.Model):
+
+    title = models.CharField(max_length=50)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Option(models.Model):
     
+    text = models.CharField(max_length=255)
+    poll = models.ForeignKey(Poll, related_name='options', on_delete=models.CASCADE)
 
-    question_text = models.CharField(max_length=200)
-    category = models.CharField(max_length=3,
-                                choices=choices,
-                                default='Other')
-    author = models.CharField(max_length=60, default='Anonymous')
-    pub_date = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.question_text
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
+        return f"{self.text} - {self.poll}"
